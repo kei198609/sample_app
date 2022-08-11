2db9f1443e0cf137b54ea4d20cacda67bb6c51a7
Ubuntu <ubuntu@ip-172-31-33-64.ap-northeast-1.compute.internal>
2022-08-11 12:20:37 +0000

    Finish ch07

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get singup_path
+    get signup_path
     assert_response :success
   end
 
