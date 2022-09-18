5117e94bf59f166d9a126a6d62fef273d3291044
Ubuntu <ubuntu@ip-172-31-33-64.ap-northeast-1.compute.internal>
2022-09-18 00:06:49 +0000

    Finish ch12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
