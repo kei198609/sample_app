cd28308edc7349dca88029e27b76b6973c5f964b
Ubuntu <ubuntu@ip-172-31-33-64.ap-northeast-1.compute.internal>
2022-10-20 23:49:01 +0000

    Finish ch14

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,22 @@
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+
+  #POST /relationships/
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+  #DELETE /relationship/:id
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+end
